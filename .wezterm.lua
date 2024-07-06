4e8f13cf36983bde9eb49bee2c58eb4c1bf32ff5
Salman Khan <salmanpathan30@gmail.com>
2024-07-06 18:10:25 +0530

    update font and font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,17 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Frappe"
-config.font = wezterm.font("CommitMono Nerd Font")
+config.font = wezterm.font("OperatorMono Nerd Font")
+config.font_size = 16
 config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 config.window_close_confirmation = "NeverPrompt"
 config.enable_tab_bar = false
+
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
 
 return config
